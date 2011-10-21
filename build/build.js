c935046098c6c1357e4ef590be96e101daeb05b5
Quyse Lert <quyse0@gmail.com>
2011-10-21 17:41:14 +0600

    windows compilation

--- build/build.js
+++ build/build.js
@@ -1,5 +1,7 @@
 var icecpp = require('ice-cpp');
 
+if(process.platform == 'win32')
+	icecpp.addEnvFile('build/env');
 icecpp.addMacro('INANITY_LIB');
 icecpp.addIncludeDir('deps/libuv/include');
 icecpp.addIncludeDir('deps/lua-5.1.4/src');
---
+++ build/clean.bat
@@ -0,0 +1,4 @@
+erase /Q debug\obj\*
+erase /Q debug\*.lib
+erase /Q release\obj\*
+erase /Q release\*.lib
