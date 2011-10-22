e40aa36bf7dec030d008a7ca309eeb56428f848a
Quyse Lert <quyse0@gmail.com>
2011-10-23 00:39:16 +0600

    using new build system

---
+++ configure.js
@@ -0,0 +1,42 @@
+exports.configureCompiler = function(objectFile, compiler) {
+	// объектные файлы: <conf>/object
+	var a = /^[^\/]+\/([^\/]+)$/.exec(objectFile);
+	// console.log('zzz', objectFile, a);
+	compiler.setSourceFile(a[1]);
+	compiler.addMacro('INANITY_LIB');
+};
+
+/**
+ * структура проекта
+ */
+var libraries = {
+	// ****** базовая библиотека
+	'libinanity2-base': {
+		objects: [
+		// совсем общее
+		'Object', 'ManagedHeap', 'Strings', 'Exception',
+		// общее: файлы и потоки
+		'File', 'EmptyFile', 'PartFile', 'MemoryFile', 'OutputStream', 'FileInputStream',
+		// файловые системы
+		'FileSystem', 'FolderFileSystem', 'Handle', 'DiskInputStream', 'DiskOutputStream']
+	},
+	// ******* сетевая библиотека
+	'libinanity2-net': {
+		objects: [
+		// базовая часть
+		'EventLoop',
+		// сокеты
+		'Socket', 'ClientSocket', 'ServerSocket',
+		// HTTP
+		'HttpClient', 'HttpResponseStream']
+	}
+};
+
+exports.configureComposer = function(libraryFile, composer) {
+	// файлы библиотек: <conf>/library
+	var a = /^([^\/]+\/)([^\/]+)$/.exec(libraryFile);
+	var confDir = a[1];
+	var library = libraries[a[2]];
+	for ( var i = 0; i < library.objects.length; ++i)
+		composer.addObjectFile(confDir + library.objects[i]);
+};
