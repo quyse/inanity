ec1a49a92802fbc44935ac0fe5d103b4ca76eff2
Quyse Lert <quyse0@gmail.com>
2011-11-08 16:13:24 +0600

    explicit check for windows

--- windows.hpp
+++ windows.hpp
@@ -1,6 +1,10 @@
 #ifndef ___INANITY_WINDOWS_HPP___
 #define ___INANITY_WINDOWS_HPP___
 
+#ifndef ___INANITY_WINDOWS
+#error This is not windows!
+#endif
+
 #define STRICT
 #ifndef NOMINMAX
 #define NOMINMAX
