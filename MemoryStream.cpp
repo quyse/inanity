f10bc52f5e12ca21d81ed77c5bdfb29a462a6227
Quyse Lert <quyse0@gmail.com>
2011-11-09 16:35:32 +0600

    MemoryStream imported

---
+++ MemoryStream.cpp
@@ -0,0 +1,65 @@
+#include "MemoryStream.hpp"
+#include "MemoryFile.hpp"
+#include "PartFile.hpp"
+
+MemoryStream::MemoryStream() : dataSize(0), lastDataSize(0), files(1)
+{
+	files[0] = NEW(MemoryFile(0x1000));
+}
+
+void MemoryStream::Write(const void* data, size_t size)
+{
+	//цикл копирования
+	while(size)
+	{
+		File* file = files[files.size() - 1];
+		size_t lastFileSize = file->GetSize();
+		//если в последнем файле есть место
+		if(lastDataSize < lastFileSize)
+		{
+			//скопировать данные
+			size_t toCopy = std::min(size, lastFileSize - lastDataSize);
+			memcpy((char*)file->GetData() + lastDataSize, data, toCopy);
+			data = (char*)data + toCopy;
+			size -= toCopy;
+			dataSize += toCopy;
+			lastDataSize += toCopy;
+		}
+		//иначе места нет
+		else
+		{
+			//выделить новый файл
+			files.push_back(NEW(MemoryFile(dataSize)));
+			lastDataSize = 0;
+		}
+	}
+}
+
+void MemoryStream::Compact()
+{
+	//если файлов больше одного
+	if(files.size() > 1)
+	{
+		//создать файл для всех данных
+		ptr<File> file = NEW(MemoryFile(dataSize));
+		//скопировать в него данные всех файлов
+		char* data = (char*)file->GetData();
+		for(size_t i = 0; i < files.size(); ++i)
+		{
+			File* file = &*files[i];
+			size_t size = (i < files.size() - 1) ? file->GetSize() : lastDataSize;
+			memcpy(data, file->GetData(), size);
+			data += size;
+		}
+		//заменить все файлы на один
+		files.assign(1, file);
+		//указать действительные размеры данных
+		lastDataSize = dataSize;
+	}
+}
+
+ptr<File> MemoryStream::ToFile()
+{
+	Compact();
+	return dataSize < files[0]->GetSize() ? NEW(PartFile(files[0], files[0]->GetData(), dataSize)) : files[0];
+}
---
+++ MemoryStream.hpp
@@ -0,0 +1,39 @@
+#ifndef ___INANITY_MEMORY_STREAM_HPP___
+#define ___INANITY_MEMORY_STREAM_HPP___
+
+#include "OutputStream.hpp"
+#include <vector>
+
+BEGIN_INANITY
+
+class File;
+
+/// Класс потока в памяти.
+/** Представляет собой расширяемый файл в памяти с интерфейсом потока. В любой момент выделено памяти
+не более, чем в два раза больше, чем размер содержимого. */
+class MemoryStream : public OutputStream
+{
+private:
+	/// Общий размер данных.
+	size_t dataSize;
+	/// Размер данных в последнем файле.
+	size_t lastDataSize;
+	/// Последовательность файлов, которые если склеить, получится результат.
+	std::vector<ptr<File> > files;
+
+	/// Сжать файлы.
+	/** То есть склеить их все и заменить одним. */
+	void Compact();
+
+public:
+	MemoryStream();
+
+	void Write(const void* data, size_t size);
+
+	/// Преобразовать в файл.
+	ptr<File> ToFile();
+};
+
+END_INANITY
+
+#endif
