64fdf225290a424b65d55fb300c83f1ca80a0703
Quyse Lert <quyse0@gmail.com>
2011-11-12 12:45:04 +0600

    inanity input header

---
+++ inanity-input.hpp
@@ -0,0 +1,12 @@
+#ifndef ___INANITY_INANITY_INPUT_HPP___
+#define ___INANITY_INANITY_INPUT_HPP___
+
+#include "input/Event.hpp"
+#include "input/State.hpp"
+#include "input/Frame.hpp"
+#include "input/Mux.hpp"
+#include "input/Manager.hpp"
+#include "input/Processor.hpp"
+#include "input/RawManager.hpp"
+
+#endif
