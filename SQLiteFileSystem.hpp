5d3c7ac82ab1cefa49080a9c8c71e13f748f0218
Quyse Lert <quyse0@gmail.com>
2012-06-11 02:09:57 +0400

    refactor a bit, added get entries FS methods

--- SQLiteFileSystem.hpp
+++ SQLiteFileSystem.hpp
@@ -18,15 +18,60 @@ class File;
 class SQLiteFileSystem : public FileSystem
 {
 private:
-	/// Открытая база данных.
-	sqlite3* db;
-	/// Statement для загрузки файла.
-	sqlite3_stmt* loadFileStmt;
-	/// Statement для сохранения файла.
-	sqlite3_stmt* saveFileStmt;
+	/// Класс, инкапсулирующий объект sqlite3.
+	class Db : public Object
+	{
+	private:
+		sqlite3* db;
 
-	// Сбросить запрос и освободить его аргументы.
-	static void ResetStmt(sqlite3_stmt* stmt);
+	public:
+		Db(sqlite3* db);
+		~Db();
+
+		operator sqlite3*() const;
+	};
+
+	/// Класс, инкапсулирующий объект sqlite3_stmt.
+	class Statement : public Object
+	{
+	private:
+		ptr<Db> db;
+		sqlite3_stmt* stmt;
+
+	public:
+		Statement(ptr<Db> db, sqlite3_stmt* stmt);
+		~Statement();
+
+		void Reset();
+		operator sqlite3_stmt*() const;
+	};
+
+	/// Класс, выполняющий сброс объекта sqlite3_stmt в деструкторе.
+	class QueryHandle
+	{
+	private:
+		ptr<Statement> statement;
+
+	public:
+		QueryHandle(ptr<Statement> statement);
+		~QueryHandle();
+	};
+
+	/// Открытая база данных.
+	ptr<Db> db;
+	/// Statement для загрузки файла.
+	mutable ptr<Statement> loadFileStmt;
+	/// Statement для сохранения файла.
+	mutable ptr<Statement> saveFileStmt;
+	/// Statement для получения файлов в каталоге.
+	mutable ptr<Statement> entriesStmt;
+	/// Statement для получения всех файлов.
+	mutable ptr<Statement> allEntriesStmt;
+
+	void ensureLoadFileStmt() const;
+	void ensureSaveFileStmt() const;
+	void ensureEntriesStmt() const;
+	void ensureAllEntriesStmt() const;
 
 	/// Хеш использованных файлов по их данным.
 	/** Используется для передачи данных файлов в SQLite,
@@ -34,10 +79,12 @@ private:
 	static std::unordered_multimap<void*, ptr<File> > files;
 	/// Критическая секция для синхронизации хеша файлов.
 	static CriticalSection filesCriticalSection;
-	// Захватить файл, чтобы он не освобождался (пока его не освободит SQLite).
+	/// Захватить файл, чтобы он не освобождался (пока его не освободит SQLite).
 	static void AcquireFile(ptr<File> file);
-	// Освободить файл по данным.
+	/// Освободить файл по данным.
 	static void FreeFile(void* data);
+
+	void GetEntries(const String& directoryName, std::vector<String>& entries, bool recursive) const;
 
 public:
 	/// Создать или открыть файловую систему в заданном файле.
@@ -46,6 +93,9 @@ public:
 
 	ptr<File> TryLoadFile(const String& fileName);
 	void SaveFile(ptr<File> file, const String& fileName);
+	void GetFileNames(std::vector<String>& fileNames) const;
+	void GetDirectoryEntries(const String& directoryName, std::vector<String>& entries) const;
+	void GetAllDirectoryEntries(const String& directoryName, std::vector<String>& entries) const;
 };
 
 END_INANITY
