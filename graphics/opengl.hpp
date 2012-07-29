9ea7026689f7ebdf74a1fefb90f56d5a670e41b7
Quyse Lert <quyse0@gmail.com>
2012-07-30 00:46:54 +0400

    opengl stuff

---
+++ graphics/opengl.hpp
@@ -0,0 +1,14 @@
+#ifndef ___INANITY_GRAPHICS_OPENGL_HPP___
+#define ___INANITY_GRAPHICS_OPENGL_HPP___
+
+#include "../Object.hpp"
+#include "glew/glew.h"
+#ifdef ___INANITY_WINDOWS
+#include "../windows.hpp"
+#include "glew/wglew.h"
+#endif
+#ifdef ___INANITY_LINUX
+#include "glew/glxew.h"
+#endif
+
+#endif
