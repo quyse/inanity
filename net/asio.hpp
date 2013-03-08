011b513c61a127394aa2e831bd366bd78ba06fbc
Quyse Lert <quyse0@gmail.com>
2013-03-08 12:16:05 +0400

    new asio header file

--- net/asio.hpp
+++ net/asio.hpp
@@ -1,6 +1,13 @@
 #ifndef ___INANITY_NET_ASIO_HPP___
 #define ___INANITY_NET_ASIO_HPP___
 
-#include "../deps/asio/include/asio.hpp"
+#include "net.hpp"
+
+#ifdef ___INANITY_WINDOWS
+#include "../windows.hpp"
+#endif
+
+#define BOOST_ASIO_SEPARATE_COMPILATION
+#include <boost/asio.hpp>
 
 #endif
