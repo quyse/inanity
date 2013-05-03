8a2a6c5ab0d7cd0efa8f585c6fa6aeaffe887d08
Quyse Lert <quyse0@gmail.com>
2013-05-04 03:33:02 +0400

    remove geometry class

--- graphics/Geometry.cpp
+++
@@ -1,9 +0,0 @@
-#include "Geometry.hpp"
-#include "../scripting_impl.hpp"
-
-BEGIN_INANITY_GRAPHICS
-
-SCRIPTABLE_MAP_BEGIN(Geometry, Inanity.Graphics.Geometry);
-SCRIPTABLE_MAP_END();
-
-END_INANITY_GRAPHICS
