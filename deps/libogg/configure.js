7bb6658b845774c9e3422aa2e7c0bc14956de0df
Quyse Lert <quyse0@gmail.com>
2013-08-21 01:09:52 +0400

    libogg configuration

---
+++ deps/libogg/configure.js
@@ -0,0 +1,33 @@
+/* конфигурационный файл libogg
+ * для компиляции в составе с inanity2
+ */
+
+var macros = [];
+
+var includes = [];
+
+exports.configureCompiler = function(objectFile, compiler) {
+	// объектные файлы: <conf>/object
+	var a = /^([^\/]+)\/([^\/]+)$/.exec(objectFile);
+	compiler.configuration = a[1];
+	compiler.setSourceFile('src/' + a[2].replace(/\./g, '/') + '.c');
+	for ( var i = 0; i < macros.length; ++i)
+		compiler.addMacro(macros[i]);
+	for ( var i = 0; i < includes.length; ++i)
+		compiler.addIncludeDir(includes[i]);
+	compiler.cppMode = false;
+};
+
+var librariesObjects = {
+	libogg: ['bitwise', 'framing']
+};
+
+exports.configureComposer = function(libraryFile, composer) {
+	// файлы библиотек: <conf>/library
+	var a = /^(([^\/]+)\/)([^\/]+)$/.exec(libraryFile);
+	var confDir = a[1];
+	composer.configuration = a[2];
+	var objects = librariesObjects[a[3]];
+	for ( var i = 0; i < objects.length; ++i)
+		composer.addObjectFile(confDir + objects[i].replace(/\//g, '.'));
+};
