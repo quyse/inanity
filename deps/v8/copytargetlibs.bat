ad0a7856a46b4c30d178f8250ddd2c990021f935
Quyse Lert <quyse0@gmail.com>
2013-09-17 21:26:30 +0000

    fix for copy libs v8 script

--- deps/v8/copytargetlibs.bat
+++ deps/v8/copytargetlibs.bat
@@ -1,4 +1,4 @@
 copy /Y repo\build\Debug\lib\v8_base.ia32.lib debug\libv8_base.lib
-copy /Y repo\build\Debug\lib\v8_base.ia32.lib debug\libv8_snapshot.lib
-copy /Y repo\build\Release\lib\v8_snapshot.lib release\libv8_base.lib
+copy /Y repo\build\Debug\lib\v8_snapshot.lib debug\libv8_snapshot.lib
+copy /Y repo\build\Release\lib\v8_base.ia32.lib release\libv8_base.lib
 copy /Y repo\build\Release\lib\v8_snapshot.lib release\libv8_snapshot.lib
