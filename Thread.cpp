e8650bd0d6d85e208c54ce4b66020d7b63237440
Quyse Lert <quyse0@gmail.com>
2013-09-18 15:47:23 +0400

    Thread::Sleep method added

--- Thread.cpp
+++ Thread.cpp
@@ -2,6 +2,7 @@
 #include "Exception.hpp"
 #ifdef ___INANITY_LINUX
 #include <pthread.h>
+#include <time.h>
 #endif
 
 BEGIN_INANITY
@@ -57,6 +58,19 @@ void Thread::WaitEnd()
 	if(pthread_join(thread, 0))
 #endif
 		THROW_SECONDARY("Can't wait for thread end", Exception::SystemError());
+}
+
+void Thread::Sleep(int milliseconds)
+{
+#ifdef ___INANITY_WINDOWS
+  ::Sleep(milliseconds);
+#endif
+#ifdef ___INANITY_LINUX
+  struct timespec t;
+  t.tv_sec = (time_t)(milliseconds / 1000);
+  t.tv_nsec = (long)((milliseconds % 1000) * 1000000);
+  nanosleep(&t, 0);
+#endif
 }
 
 END_INANITY
