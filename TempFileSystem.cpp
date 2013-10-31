7d12cc027f3937527e9767581a400ef4a7c7b254
Quyse Lert <quyse0@gmail.com>
2013-10-31 21:15:44 +0000

    remove old code from TempFileSystem

--- TempFileSystem.cpp
+++ TempFileSystem.cpp
@@ -3,8 +3,6 @@
 
 BEGIN_INANITY
 
-String TempFileSystem::staticResourceName = "/inanity/tempfs";
-
 ptr<File> TempFileSystem::TryLoadFile(const String& fileName)
 {
 	std::unordered_map<String, ptr<File> >::const_iterator i = files.find(fileName);
@@ -14,11 +12,6 @@ ptr<File> TempFileSystem::TryLoadFile(const String& fileName)
 void TempFileSystem::SaveFile(ptr<File> file, const String& fileName)
 {
 	files[fileName] = file;
-}
-
-ptr<TempFileSystem> TempFileSystem::CreateStaticResource(ResourceLoader* resourceLoader)
-{
-	return NEW(TempFileSystem());
 }
 
 END_INANITY
--- TempFileSystem.hpp
+++ TempFileSystem.hpp
@@ -12,17 +12,12 @@ class ResourceLoader;
 /** Просто хеш с файлами. Позволяет добавлять файлы во время работы. */
 class TempFileSystem : public FileSystem
 {
-public:
-	static String staticResourceName;
-
 private:
 	std::unordered_map<String, ptr<File> > files;
 
 public:
 	ptr<File> TryLoadFile(const String& fileName);
 	void SaveFile(ptr<File> file, const String& fileName);
-
-	static ptr<TempFileSystem> CreateStaticResource(ResourceLoader* resourceLoader);
 };
 
 END_INANITY
