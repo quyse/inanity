181db4b280a246d73541467b42db094b7c445057
Quyse Lert <quyse0@gmail.com>
2013-12-23 19:31:13 +0000

    http-parser build configuration

---
+++ deps/http-parser/configure.js
@@ -0,0 +1,23 @@
+/* configure for building http-parser
+ */
+
+exports.configureCompiler = function(objectFile, compiler) {
+	// object files: <conf>/object
+	var a = /^([^\/]+)\/([^\/]+)$/.exec(objectFile);
+	compiler.configuration = a[1];
+	compiler.setSourceFile('repo/' + a[2] + '.c');
+	compiler.cppMode = false;
+	compiler.addIncludeDir('../zlib');
+	compiler.strict = false;
+};
+
+var objects = ['http_parser'];
+
+exports.configureComposer = function(libraryFile, composer) {
+	// library files: <conf>/library
+	var a = /^(([^\/]+)\/)([^\/]+)$/.exec(libraryFile);
+	var confDir = a[1];
+	composer.configuration = a[2];
+	for(var i = 0; i < objects.length; ++i)
+		composer.addObjectFile(confDir + objects[i]);
+};
