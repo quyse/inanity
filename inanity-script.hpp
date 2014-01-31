143ac5e9d3cfa3667de1ccb779f6dd78fce517de
Quyse Lert <quyse0@gmail.com>
2014-01-31 16:41:08 +0000

    added inanity-script.hpp

---
+++ inanity-script.hpp
@@ -0,0 +1,8 @@
+#ifndef ___INANITY_INANITY_SCRIPT_HPP___
+#define ___INANITY_INANITY_SCRIPT_HPP___
+
+#include "script/Any.hpp"
+#include "script/Function.hpp"
+#include "script/State.hpp"
+
+#endif
