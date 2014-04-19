2745077ce10e2a14e0d4fba84f75e8f10da2a2e8
Quyse Lert <quyse0@gmail.com>
2014-04-19 18:23:44 +0000

    remove old data/oil code

--- data/oil/oil.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ___INANITY_DATA_OIL_OIL_HPP___
-#define ___INANITY_DATA_OIL_OIL_HPP___
-
-#include "../data.hpp"
-
-#define BEGIN_INANITY_OIL BEGIN_INANITY_DATA namespace Oil {
-#define END_INANITY_OIL } END_INANITY_DATA
-
-#endif
