30a70ef2a3138fcdd3007bb7870cb6818ab92231
Quyse Lert <quyse0@gmail.com>
2015-11-29 15:57:39 +0300

    don't restrict pixel sizes in SDL video init

--- platform/Sdl.cpp
+++ platform/Sdl.cpp
@@ -48,12 +48,7 @@ ptr<Sdl> Sdl::Get(Uint32 flags)
 			{
 				{ SDL_GL_CONTEXT_MAJOR_VERSION, 3 },
 				{ SDL_GL_CONTEXT_MINOR_VERSION, 3 },
-				{ SDL_GL_DOUBLEBUFFER, 1 },
-				{ SDL_GL_RED_SIZE, 8 },
-				{ SDL_GL_GREEN_SIZE, 8 },
-				{ SDL_GL_BLUE_SIZE, 8 },
-				{ SDL_GL_DEPTH_SIZE, 0 },
-				{ SDL_GL_STENCIL_SIZE, 0 }
+				{ SDL_GL_DOUBLEBUFFER, 1 }
 			};
 			for(size_t i = 0; i < sizeof(attrs) / sizeof(attrs[0]); ++i)
 				if(SDL_GL_SetAttribute(attrs[i].attr, attrs[i].value) != 0)
