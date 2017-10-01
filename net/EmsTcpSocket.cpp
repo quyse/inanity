02f1031e139d540ebd2ebffbc4138a575c3382f3
Quyse Lert <quyse0@gmail.com>
2017-10-01 18:20:00 +0300

    closing EmsTcpSocket

--- net/EmsTcpSocket.cpp
+++ net/EmsTcpSocket.cpp
@@ -2,6 +2,7 @@
 #include "EmsService.hpp"
 #include "../MemoryFile.hpp"
 #include <sys/socket.h>
+#include <unistd.h>
 
 BEGIN_INANITY_NET
 
@@ -13,11 +14,12 @@ EmsTcpSocket::EmsTcpSocket(ptr<EmsService> service, int fd, ptr<Service::TcpSock
 
 EmsTcpSocket::~EmsTcpSocket()
 {
-	service->UnregisterTcpSocket(fd);
+	Close();
 }
 
 void EmsTcpSocket::Send(ptr<File> file, ptr<SendHandler> sendHandler)
 {
+	if(!fd) return;
 	send(fd, file->GetData(), file->GetSize(), 0);
 	if(sendHandler)
 		sendHandler->FireSuccess();
@@ -38,6 +40,10 @@ void EmsTcpSocket::SetNoDelay(bool noDelay)
 
 void EmsTcpSocket::Close()
 {
+	if(!fd) return;
+	close(fd);
+	service->UnregisterTcpSocket(fd);
+	fd = 0;
 }
 
 void EmsTcpSocket::OnOpen()
@@ -53,6 +59,7 @@ void EmsTcpSocket::OnOpen()
 
 void EmsTcpSocket::OnMessage()
 {
+	if(!fd) return;
 	uint8_t buf[4096];
 	ssize_t len;
 	while((len = recv(fd, buf, sizeof(buf), 0)) > 0)
