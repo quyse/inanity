5feca2feea74b8f769b2529800378ef4761eddba
Alexander Bich <quyse0@gmail.com>
2020-12-28 20:07:39 +0300

    upgrade harfbuzz to 2.7.4

--- deps/harfbuzz/configure.js
+++ deps/harfbuzz/configure.js
@@ -10,7 +10,9 @@ exports.configureCompiler = function(objectFile, compiler) {
 	compiler.addIncludeDir('repo/src');
 	compiler.addIncludeDir('../ucdn/repo');
 	compiler.addIncludeDir('generated');
+	compiler.addMacro('HB_NO_MT');
 	compiler.addMacro('HAVE_OT');
+	compiler.addMacro('HAVE_FREETYPE');
 	compiler.addMacro('HAVE_UCDN');
 	// crazy hacks for xbox
 	if(compiler.platform == 'xbox') {
@@ -22,38 +24,59 @@ exports.configureCompiler = function(objectFile, compiler) {
 
 var objects = [
 	// base
+	'hb-aat-layout',
+	'hb-aat-map',
 	'hb-blob',
-	'hb-buffer-serialize',
 	'hb-buffer',
+	'hb-buffer-serialize',
 	'hb-common',
-	// 'hb-coretext',
+	'hb-coretext',
+	'hb-draw',
 	'hb-face',
-	// 'hb-fallback-shape',
+	'hb-fallback-shape',
 	'hb-font',
+	'hb-gdi',
+	'hb-map',
+	'hb-number',
 	'hb-set',
 	'hb-shape',
 	'hb-shape-plan',
 	'hb-shaper',
-	//'hb-tt-font',
+	'hb-static',
+	'hb-style',
+	'hb-subset',
+	'hb-subset-cff-common',
+	'hb-subset-cff1',
+	'hb-subset-cff2',
+	'hb-subset-input',
+	'hb-subset-plan',
+	'hb-ucd',
 	'hb-unicode',
-	'hb-ucdn',
-	// 'hb-warning',
 
 	// opentype
+	'hb-ot-cff1-table',
+	'hb-ot-cff2-table',
+	'hb-ot-color',
+	'hb-ot-face',
+	'hb-ot-font',
 	'hb-ot-layout',
 	'hb-ot-map',
+	'hb-ot-meta',
+	'hb-ot-metrics',
+	'hb-ot-name',
 	'hb-ot-shape',
 	'hb-ot-shape-complex-arabic',
 	'hb-ot-shape-complex-default',
 	'hb-ot-shape-complex-hangul',
 	'hb-ot-shape-complex-hebrew',
-	'hb-ot-shape-complex-indic-table',
 	'hb-ot-shape-complex-indic',
+	'hb-ot-shape-complex-indic-table',
+	'hb-ot-shape-complex-khmer',
 	'hb-ot-shape-complex-myanmar',
 	'hb-ot-shape-complex-thai',
-	'hb-ot-shape-complex-tibetan',
 	'hb-ot-shape-complex-use',
 	'hb-ot-shape-complex-use-table',
+	'hb-ot-shape-complex-vowel-constraints',
 	'hb-ot-shape-fallback',
 	'hb-ot-shape-normalize',
 	'hb-ot-tag',
