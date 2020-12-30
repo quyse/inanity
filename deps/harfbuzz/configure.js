fea8ec53ddc7e4eba02a74ac017be3cd490a2050
Quyse Lert <quyse0@gmail.com>
2020-12-30 14:17:59 +0300

    build harfbuzz via amalgamation

--- deps/harfbuzz/configure.js
+++ deps/harfbuzz/configure.js
@@ -24,67 +24,8 @@ exports.configureCompiler = function(objectFile, compiler) {
 };
 
 var objects = [
-	// base
-	'hb-aat-layout',
-	'hb-aat-map',
-	'hb-blob',
-	'hb-buffer',
-	'hb-buffer-serialize',
-	'hb-common',
-	'hb-coretext',
-	'hb-draw',
-	'hb-face',
-	'hb-fallback-shape',
-	'hb-font',
-	'hb-gdi',
-	'hb-map',
-	'hb-number',
-	'hb-set',
-	'hb-shape',
-	'hb-shape-plan',
-	'hb-shaper',
-	'hb-static',
-	'hb-style',
-	'hb-subset',
-	'hb-subset-cff-common',
-	'hb-subset-cff1',
-	'hb-subset-cff2',
-	'hb-subset-input',
-	'hb-subset-plan',
-	'hb-ucd',
-	'hb-unicode',
-
-	// opentype
-	'hb-ot-cff1-table',
-	'hb-ot-cff2-table',
-	'hb-ot-color',
-	'hb-ot-face',
-	'hb-ot-font',
-	'hb-ot-layout',
-	'hb-ot-map',
-	'hb-ot-meta',
-	'hb-ot-metrics',
-	'hb-ot-name',
-	'hb-ot-shape',
-	'hb-ot-shape-complex-arabic',
-	'hb-ot-shape-complex-default',
-	'hb-ot-shape-complex-hangul',
-	'hb-ot-shape-complex-hebrew',
-	'hb-ot-shape-complex-indic',
-	'hb-ot-shape-complex-indic-table',
-	'hb-ot-shape-complex-khmer',
-	'hb-ot-shape-complex-myanmar',
-	'hb-ot-shape-complex-thai',
-	'hb-ot-shape-complex-use',
-	'hb-ot-shape-complex-use-table',
-	'hb-ot-shape-complex-vowel-constraints',
-	'hb-ot-shape-fallback',
-	'hb-ot-shape-normalize',
-	'hb-ot-tag',
-	'hb-ot-var',
-
-	// freetype
-	'hb-ft',
+	// amalgamation file is available since Harfbuzz 2.6.0
+	'harfbuzz'
 ];
 
 exports.configureComposer = function(libraryFile, composer) {
